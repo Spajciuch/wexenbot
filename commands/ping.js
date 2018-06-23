3477711d8ba74381c38120e35c6a480b6dcee9dc
Spyte <40525440+Spajciuch@users.noreply.github.com>
2018-06-23 21:35:47 +0200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -4,5 +4,6 @@ module.exports.run = async (client, message, args) => {
     m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
 }
 module.exports.help = {
-  name: "ping"
+  name: "ping",
+  category: "informacyjne"
 }
