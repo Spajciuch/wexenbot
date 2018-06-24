f2596f974ef3a1e5a91c47f7ead9c19ac58a8621
neoney <30625554+heheszeq@users.noreply.github.com>
2018-06-24 09:40:04 +0200

    Create invite.js

---
+++ commands/invite.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js')
+module.exports.run = async (client, message, args) => {
+message.author.send('https://discordapp.com/oauth2/authorize?&client_id=460153151073288202&scope=bot&permissions=8')
+message.reply("Check DMs!")
+.then(message => message.delete(5000));
+}
+module.exports.help = {
+  name: "invite",
+  category: "info"
+}
