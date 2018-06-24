677893077f2b5d7ecc79ece22f0e7ab33e209325
neoney <30625554+heheszeq@users.noreply.github.com>
2018-06-24 19:44:00 +0200

    Create unmute.js

---
+++ commands/unmute.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js')
+module.exports.run = async (client, message, args) => {
+const member = message.mentions.members.first()
+if(!member) return message.reply('You have to mention someone.');
+return member.setMute(false)
+ .then(message.reply('unmuted succesfully!'))
+ .catch(error => message.reply("couldn't unmute, because: " + error.message));
+}
+module.exports.help = {
+  name: "unmute",
+  category: "util"
+}
