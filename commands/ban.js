3f339c50a1a03f3ab65c2cbf87119879b8ab8bff
neoney <30625554+heheszeq@users.noreply.github.com>
2018-06-24 21:57:18 +0200

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,24 +1,24 @@
 const Discord = require('discord.js')
 module.exports.run = async (client, message, args, config) => {
     if (!message.member.hasPermission("BAN_MEMBERS", false, true, true))
-      return message.reply("You don't have enough permissions!");
+      return message.reply("<a:banhammer:460519287191240714> You don't have enough permissions!");
 
     let member = message.mentions.members.first();
     if (!member)
-      return message.reply("You have to mention someone.");
+      return message.reply("<a:banhammer:460519287191240714> You have to mention someone.");
     if (!member.bannable)
-      return message.reply("I can't do that! Do I have enough permissions?");
+      return message.reply("<a:banhammer:460519287191240714> I can't do that! Do I have enough permissions?");
 
     let reason = args.slice(1).join(' ');
     if (!reason)
-      return message.reply("Give me a reason!");
+      return message.reply("<a:banhammer:460519287191240714> Give me a reason!");
 
-    await member.send("You've been banned on **" + message.guild.name + "**, because " + reason + ". You've been banned by " + message.author.tag + " (id: " + message.author.id + ").")
+    await member.send("<a:banhammer:460519287191240714> You've been banned on **" + message.guild.name + "**, because " + reason + ". You've been banned by " + message.author.tag + " (id: " + message.author.id + ").")
     .then(member.ban(7, reason))
-      .catch(error => message.reply(`Sorry ${message.author} , i can't ban, because: ${error.code}`));
+      .catch(error => message.reply(`<a:banhammer:460519287191240714> Sorry ${message.author} , i can't ban, because: ${error.code}`));
     const embed = {
   "title": "Banned!",
-  "description": `${member.user.tag} (id: ${member.user.id}) has been banned by ${message.author.tag} (id: ${message.author.id}), because: ${reason}`,
+  "description": `<a:banhammer:460519287191240714> ${member.user.tag} (id: ${member.user.id}) has been banned by ${message.author.tag} (id: ${message.author.id}), because: ${reason}`,
   "color": 16750361,
   "footer": {
     "icon_url": "https://cdn.discordapp.com/avatars/460153151073288202/e6fb8a855d1a0646bf790cfe3022e69a.png?size=2048",
