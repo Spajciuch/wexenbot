b51f61f1bc4950b1ff266ba840b3dd4a114e45b5
neoney <30625554+heheszeq@users.noreply.github.com>
2018-06-24 19:41:58 +0200

    Create mute.js

---
+++ commands/mute.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js')
+module.exports.run = async (client, message, args) => {
+const member = message.mentions.members.first()
+if(!member) return message.reply('You have to mention someone.');
+const reason = args.slice(1).join(' ');
+if(!reason) return message.reply('Give me a reason!');
+return member.setMute(true, reason)
+ .then(message.reply('Muted succesfully!'))
+ .catch(error => message.reply("couldn't ban, because: " + error.message));
+}
+module.exports.help = {
+  name: "mute",
+  category: "util"
+}
