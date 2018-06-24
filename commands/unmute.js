b90a54adc5d839c4217392ef8bf3c5998c7449d9
neoney <30625554+heheszeq@users.noreply.github.com>
2018-06-24 21:26:05 +0200

    Update unmute.js

--- commands/unmute.js
+++ commands/unmute.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js')
-module.exports.run = async (client, message, args) => {
+module.exports.run = async (client, message, args, config) => {
 const member = message.mentions.members.first()
 if(!member) return message.reply('You have to mention someone.');
 return member.setMute(false)
