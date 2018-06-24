2b4c1462fcbdfc31aab869e034da845ce1dad8c3
neoney <30625554+heheszeq@users.noreply.github.com>
2018-06-24 19:36:07 +0200

    Delete newwebhook.js

--- commands/newwebhook.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js')
-module.exports.run = async (client, message, args) => {
- let argi = args.join(" ").split(" | ")
-message.channel.createWebhook(argi[0], argi[1])
-}
-module.exports.help = {
-  name: "newwebhook",
-  category: "util"
-}
