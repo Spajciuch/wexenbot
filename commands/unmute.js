5c3ceb5110efbd9761ad3c3e12f31a3de0d06c2d
Spyte <40525440+Spajciuch@users.noreply.github.com>
2018-06-24 22:31:30 +0200

    Update unmute.js

--- commands/unmute.js
+++ commands/unmute.js
@@ -1,10 +1,37 @@
 const Discord = require('discord.js')
 module.exports.run = async (client, message, args, config) => {
-const member = message.mentions.members.first()
-if(!member) return message.reply('You have to mention someone.');
-return member.setMute(false)
- .then(message.reply('unmuted succesfully!'))
- .catch(error => message.reply("couldn't unmute, because: " + error.message));
+  if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You aren't permitted to do that");
+    let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if (!tomute) return message.reply("Couldn't find user");
+    let muterole = message.guild.roles.find(`name`, "Muted");
+    if (!muterole) {
+        try {
+            muterole = await message.guild.createRole({
+                name: "Muted",
+                color: "#000000",
+                permissions: []
+            })
+            message.guild.channels.forEach(async (channel, id) => {
+                await channel.overwritePermissions(muterole, {
+                    SEND_MESSAGES: false,
+                    ADD_REACTIONS: false
+                });
+            });
+        } catch (e) {
+            console.log(e.stack);
+        }
+    }
+tomute.removeRole(muterole.id);
+message.channel.send(`<@${tomute.id}> has been unmuted!`);
+
+
+}
+
+exports.conf = {
+    aliases: [],
+    permLevel: 2
+};
+
 }
 module.exports.help = {
   name: "unmute",
