ce6085c49093e549ee7a1a58b5c2916e54878012
Spyte <40525440+Spajciuch@users.noreply.github.com>
2018-06-25 22:03:06 +0200

    Create userinfo.js

---
+++ userinfo.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+const moment = require("moment");
+
+module.exports.run = async (client, message, args, config) => {
+	let user;
+    if (message.mentions.users.first()) {
+      user = message.mentions.users.first();
+    } else {
+        user = message.author;
+    }
+    const member = message.guild.member(user);
+	
+    const embed = new Discord.RichEmbed()
+		.setColor(config.embed_color)
+		.setThumbnail(user.avatarURL)
+		.setTitle(`${user.username}#${user.discriminator}`)
+		.addField("ID:", `${user.id}`, true)
+		.addField("Nickname:", `${member.nickname !== null ? `${member.nickname}` : 'None'}`, true)
+		.addField("Created At:", `${moment.utc(user.createdAt).format('dddd, MMMM Do YYYY, HH:mm:ss')}`, true)
+		.addField("Joined Server:", `${moment.utc(member.joinedAt).format('dddd, MMMM Do YYYY, HH:mm:ss')}`, true)
+		.addField("Bot:", `${user.bot}`, true)
+		.addField("Status:", `${user.presence.status}`, true)
+		.addField("Game:", `${user.presence.game ? user.presence.game.name : 'None'}`, true)
+		.addField("Roles:", member.roles.map(roles => `${roles.name}`).join(', '), true)
+		.setFooter(`Replying to ${message.author.username}#${message.author.discriminator}`)
+     message.channel.send({embed});
+    }
+};
+module.exports.help = { 
+  name: "userinfo",
+  category: "info"
+}
