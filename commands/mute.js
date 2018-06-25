4507413ac3955e2bb443e5c619bd41d5ff3e3431
Spyte <40525440+Spajciuch@users.noreply.github.com>
2018-06-25 15:31:11 +0200

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -1,14 +1,41 @@
-const Discord = require('discord.js')
-module.exports.run = async (client, message, args, config) => {
-const member = message.mentions.members.first()
-if(!member) return message.reply('You have to mention someone.');
-const reason = args.slice(1).join(' ');
-if(!reason) return message.reply('Give me a reason!');
-return member.setMute(true, reason)
- .then(message.reply('Muted succesfully!'))
- .catch(error => message.reply("couldn't mute, because: " + error.message));
+const Discord = require("discord.js");
+const ms = require("ms");
+module.exports.run = async (bot, message, args, config) => {
+
+      if (!message.member.hasPermission("KICK_MEMBERS")) return message.reply("You aren't permitted to do that");
+    let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if (!tomute) return message.reply("Couldn't find user.");
+    let muterole = message.guild.roles.find(`name`, "Muted");
+
+    if (!muterole) {
+        try {
+            muterole = await message.guild.createRole({
+                name: "Muted",
+                color: "#000000",
+                permissions: []
+            })
+            message.guild.channels.forEach(async (channel, id) => {
+                await channel.overwritePermissions(muterole, {
+                    SEND_MESSAGES: false,
+                    ADD_REACTIONS: false
+                });
+            });
+        } catch (e) {
+            console.log(e.stack);
+        }
+    }
+    if(tomute.highestRole.position > message.member.highestRole.position) return message.reply('He/She has a higher role than you!');
+
+    await (tomute.addRole(muterole.id));
+    message.reply(`<@${tomute.id}> has been muted`);
 }
+
+exports.conf = {
+    aliases: [],
+    permLevel: 2
+};
+
 module.exports.help = {
-  name: "mute",
-  category: "util"
+    name: "mute",
+    category: "util"
 }
