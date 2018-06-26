b78aa69795b6d654634943d8811194804cc28b42
neoney <30625554+heheszeq@users.noreply.github.com>
2018-06-26 21:23:17 +0200

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js')
 module.exports.run = async (client, message, args, config) => {
 var firebase = require('firebase')
-firebase.database().ref(`/ustawienia/${message.guild.id}/admin`).once('value')
-.then(async function admin => { if(admin.val() == false) return message.reply('Module is off.')
+await firebase.database().ref(`/ustawienia/${message.guild.id}/admin`).once('value')
+.then(admin => { if(admin.val() == false) return message.reply('Module is off.') })
     if (!message.member.hasPermission("BAN_MEMBERS", false, true, true))
       return message.reply("<a:banhammer:460519287191240714> You don't have enough permissions!");
 
@@ -33,7 +33,6 @@ firebase.database().ref(`/ustawienia/${message.guild.id}/admin`).once('value')
 };
 message.channel.send({ embed });
 }
-      })
 module.exports.help = {
   name: "ban",
   category: "util"
