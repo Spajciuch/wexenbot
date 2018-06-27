f4d3d9b810e655d8b3f1588569edac3f746ba0e8
Spyte <40525440+Spajciuch@users.noreply.github.com>
2018-06-27 12:28:51 +0200

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -14,35 +14,18 @@ await firebase.database().ref(`/ustawienia/${message.guild.id}/admin`).once('val
 
     let reason = args.slice(1).join(' ');
     if (!reason)
-      return message.reply("<a:banhammer:460519287191240714> Give me a reason!");
+      reason = "Not specified reason"
 
     await member.send("<a:banhammer:460519287191240714> You've been banned on **" + message.guild.name + "**, because " + reason + ". You've been banned by " + message.author.tag + " (id: " + message.author.id + ").")
     .then(member.ban(7, reason))
       .catch(error => message.reply(`<a:banhammer:460519287191240714> Sorry ${message.author} , i can't ban, because: ${error.code}`));
-    const embed = {
-  "title": "Banned!",
-  "color": 16750361,
-  "thumbnail": {
-    "url": message.mentions.members.first().user.avatarURL
-  },
-   "fields": [
-      {
-        "name": "Member",
-        "value": member.user.tag
-      },
-      {
-          "name":"Banned By",
-          "value": message.author
-      },
-       {
-           "name": "Reason",
-           "value": reason
-      }
-    ]
-  }
-      message.channel.send({ embed });
-})
-     
+   let embed = new Discord.RichEmbed()
+   .setTitle("Banned!")
+   .setColor(config.embed_color)
+   .addField("Member", member)
+   .addField("Banned By",message.author)
+   .addField("Reason",reason)
+   message.channel.send({embed})
 }
 module.exports.help = {
   name: "ban",
