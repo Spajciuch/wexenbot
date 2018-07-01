c9c79e2e38b0b78cd831a964d4b7fc39737d13f9
neoney <30625554+heheszeq@users.noreply.github.com>
2018-07-01 18:55:08 +0200

    Update translate.js

--- commands/translate.js
+++ commands/translate.js
@@ -32,8 +32,9 @@ module.exports.run = async (client, message, args, config) => {
                 .addField("to: " + to_language, res.text)
                 .setThumbnail("https://cdn.dribbble.com/users/1341307/screenshots/3641494/google_translate.gif") // Optionnal stuff
             message.channel.send(translateembed)
-        }).catch(err => {
-            message.channel.send(":x: Usage: `" + config.prefix + "translate [from:iso] [to:iso] <some text>` \nThe from: and to: are optional, you can check out <http://bit.ly/ISO_codesWiki> for the iso codes\nExample: ```" + config.prefix + "translate from:ro to:fr Salut, ce mai faci?```") // Yes, I used Romanian for my example. Do you have any problem?
+        })
+        .catch(err => {
+            message.channel.send("AAAA:x: Usage: `" + config.prefix + "translate [from:iso] [to:iso] <some text>` \nThe from: and to: are optional, you can check out <http://bit.ly/ISO_codesWiki> for the iso codes\nExample: ```" + config.prefix + "translate from:ro to:fr Salut, ce mai faci?```") // Yes, I used Romanian for my example. Do you have any problem?
         });
     } else {
         message.channel.send(":x: Usage: `" + config.prefix + "translate [from:iso] [to:iso] <some text>` \nThe from: and to: are optional, you can check out <http://bit.ly/ISO_codesWiki> for the iso codes\nExample: ```" + config.prefix + "translate from:ro to:fr Salut, ce mai faci?```")
