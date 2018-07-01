afd9121ef47571633a6ca454f512d0c5377e30e8
Spyte <40525440+Spajciuch@users.noreply.github.com>
2018-07-01 18:14:02 +0200

    Update translate.js

--- commands/translate.js
+++ commands/translate.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js')
 const config = require('../config.json')
-module.exports.run = async (bot, message, args) => {
+module.exports.run = async (client, message, args) => {
   const translate = require('google-translate-api');
     if (args[0]) {
         let from_language = "auto" // default languages
