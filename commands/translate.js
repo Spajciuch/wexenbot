7a9219c669be62fa6121f78388cc7534c2ea5ef3
neoney <30625554+heheszeq@users.noreply.github.com>
2018-07-01 18:31:44 +0200

    Update translate.js

--- commands/translate.js
+++ commands/translate.js
@@ -4,35 +4,32 @@ module.exports.run = async (client, message, args, config) => {
     if (args[0]) {
         let from_language = "auto" // default languages
         let to_language = "en" // default languages
-        let tobe_translated = args.slice(2).join(" ") // Getting the text
+        var tobe_translated = "";
         if (args[0].startsWith("from:")) { // Checking if there is a from:language & to:language, this part is not optimized
             from_language = args[0].slice(5)
-            tobe_translated = tobe_translated.slice(args[0].length + 1)
+            tobe_translated = args.slice(1).join(" ")
             if (args[1].startsWith("to:")) {
                 to_language = args[1].slice(3)
-                tobe_translated = tobe_translated.slice(args[1].length + 1) // cutting the from & to from the text
+                tobe_translated = args.slice(2).join(" ")
             }
         } else if (args[0].startsWith("to:")) { // Checking if there is a to:language & from:language, Yes I check 2 times :/
             to_language = args[0].slice(3)
-            tobe_translated = tobe_translated.slice(args[0].length + 1)
+            tobe_translated = args.slice(1).join(" ")
             if (args[1].startsWith("from:")) {
                 from_language = args[1].slice(5)
-                tobe_translated = tobe_translated.slice(args[1].length + 1) // cutting the from & to from the text
+                tobe_translated = args.slice(2).join(" ")
             }
         }
-        translate(tobe_translated, {
-            from: from_language,
-            to: to_language
-        }).then(res => { // We translate the text
+        translate(tobe_translated, {from: from_language, to: to_language})
+          .then(res => {
             from_language = res.from.language.iso
             if (res.from.text.value) tobe_translated = res.from.text.value
-            final_text = res.text
             let translateembed = new Discord.RichEmbed()
                 .setTitle("Translate") // Optionnal stuff
                 .setColor(`0x3980b3`) // Optionnal stuff
                 .setDescription(final_text) // Optionnal stuff
-                .addField("`from: " + from_language + "`", "```" + tobe_translated + "```")
-                .addField("`to: " + to_language + "`", "```" + final_text + "```")
+                .addField("from: " + from_language, tobe_translated +)
+                .addField("to: " + to_language, res.text)
                 .setThumbnail("https://cdn.dribbble.com/users/1341307/screenshots/3641494/google_translate.gif") // Optionnal stuff
             message.channel.send(translateembed)
         }).catch(err => {
