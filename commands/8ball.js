b86bd47a658a4bb92d85ee1be63bb796f324b488
neoney <30625554+heheszeq@users.noreply.github.com>
2018-07-02 17:56:50 +0200

    hotfix - 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -3,7 +3,7 @@ module.exports.run = async (client, message, args, config) => {
 message.reply("It's in build.")
 const eightball = require('@spyte-corp/simple-8ball-options')
 const options = ["Yes", "No", "I don't know!", "Ask Spyte!", "Ask neoney!", "Maybe", "It's possible!"]
-message.reply(eightball(options)))
+message.reply(eightball(options))
 }
 module.exports.help = {
   name: "8ball",
