d46e434150a19f1148f31f19d310d854aa88fa80
neoney <30625554+heheszeq@users.noreply.github.com>
2018-07-02 21:53:48 +0200

    8ball is out! ^^

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,9 +1,8 @@
 const Discord = require('discord.js')
 module.exports.run = async (client, message, args, config) => {
-message.reply("It's in build.")
 var simple8ballOptions = require("@spyte-corp/simple-8ball-options")
 let options = ["No", "Yes", "I don't know!", "Ask someone else!"]
-console.log(simple8ballOptions(options))
+message.channel.send(simple8ballOptions(options))
 }
 module.exports.help = {
   name: "8ball",
