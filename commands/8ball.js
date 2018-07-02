ad26fb195ab42a3751525102acedfa8072505eb4
neoney <30625554+heheszeq@users.noreply.github.com>
2018-07-02 17:52:09 +0200

    8ball in test phase

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,6 +1,9 @@
 const Discord = require('discord.js')
 module.exports.run = async (client, message, args, config) => {
 message.reply("It's in build.")
+const eightball = require('@spyte-corp/simple-8ball-options')
+const options = ["Yes", "No", "I don't know!", "Ask Spyte!", "Ask neoney!", "Maybe", "It's possible!"]
+message.reply(eightball(options)))
 }
 module.exports.help = {
   name: "8ball",
