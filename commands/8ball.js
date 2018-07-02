c208bdc7888f5964001130489bd0c0feb44879ce
neoney <30625554+heheszeq@users.noreply.github.com>
2018-07-02 17:46:42 +0200

    More info on 8ball

---
+++ commands/8ball.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js')
+module.exports.run = async (client, message, args, config) => {
+message.reply("It's in build.")
+}
+module.exports.help = {
+  name: "8ball",
+  category: "fun"
+}
