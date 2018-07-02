3e6d72f00e3b4f2d4f113cf549432184dc259958
Sprite1234 <37411789+Sprite1234@users.noreply.github.com>
2018-07-02 10:27:37 +0200

    Update z Atoma :D

--- commands/say.js
+++ commands/say.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js')
 module.exports.run = async (client, message, args, config) => {
 message.channel.send(args.join(" "));
+message.delete()
 }
 module.exports.help = {
   name: "say",
