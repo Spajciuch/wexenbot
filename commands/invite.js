b2fcf6eabb57f1545776111c3ef33b5eca3db123
neoney <30625554+heheszeq@users.noreply.github.com>
2018-07-04 21:30:18 +0200

    4 plik

--- commands/invite.js
+++ commands/invite.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js')
 module.exports.run = async (client, message, args, config) => {
-message.author.send('https://discordapp.com/oauth2/authorize?&client_id=460153151073288202&scope=bot&permissions=8')
+message.author.send('<https://discordapp.com/oauth2/authorize?&client_id=460153151073288202&scope=bot&permissions=8>')
 message.reply("Check DMs!")
 .then(message => message.delete(5000));
 }
