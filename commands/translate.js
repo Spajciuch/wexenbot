ad625fcc477c6e009615bd08b8c830d68bc9e9d7
neoney <30625554+heheszeq@users.noreply.github.com>
2018-07-04 21:30:27 +0200

    6 plik

--- commands/translate.js
+++ commands/translate.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 module.exports.run = async (client, message, args, config) => {
   const translate = require('google-translate-api');
     if (args[0]) {
-        let from_language = "auto" 
+        let from_language = "auto"
         let to_language = "en"
 
         var tobe_translated = "";
@@ -30,6 +30,8 @@ module.exports.run = async (client, message, args, config) => {
                 .setColor(config.embed_color) // Optionnal stuff
                 .addField("from: " + from_language, tobe_translated)
                 .addField("to: " + to_language, res.text)
+                .setFooter("weXen", config.avatar_url)
+                .setTimestamp()
                 .setThumbnail("https://cdn.dribbble.com/users/1341307/screenshots/3641494/google_translate.gif") // Optionnal stuff
             message.channel.send(translateembed)
         })
