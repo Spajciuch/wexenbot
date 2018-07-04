6f81234917c9eece3ccc23cc1ec066cfe1374934
neoney <30625554+heheszeq@users.noreply.github.com>
2018-07-04 23:00:49 +0200

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -1,9 +1,10 @@
 const Discord = require('discord.js')
 module.exports.run = async (client, message, args, config) => {
+const rrm = require('@spyte-corp/discord.js-remove-on-reaction')
 if (!config.owner_ids.some(x=>x == message.author.id)) return message.reply('You can\'t do that!');
 var input = args.join(" ");
 var result = eval(input);
-message.channel.send('Input:\n```js\n' + input + '\n```\nOutput:\n```\n' + result + '\n```')
+message.channel.send('Input:\n```js\n' + input + '\n```\nOutput:\n```\n' + result + '\n```').then(msg => rrm(message, msg, false))
 }
 module.exports.help = {
   name: "eval",
