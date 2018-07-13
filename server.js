a62679291a870faa76a3110bc6cf0cdc8fdb3f20
neoney <30625554+heheszeq@users.noreply.github.com>
2018-07-13 21:30:53 +0200

    Maybe fixed, maybe broken

--- server.js
+++ server.js
@@ -233,14 +233,13 @@ const music = new Music(client, {
   djRole: "@everyone"
 });
 
-
-setInterval(function () {
-database.ref("/ustawienia/admin/on").once('value')
-    .then(result => {
-      if(result.val() == false) {
-        client.destroy(process.env.TOKEN)
-      } else {
-        client.login(process.env.TOKEN)
-      }
-    })
-}, 10000);
+var on = database.ref('/ustawienia/admin/on');
+on.on('value', function(result) {
+  setInterval(function () {
+    if(result.val() == false) {
+     client.destroy(process.env.TOKEN)
+    } else {
+     client.login(process.env.TOKEN)
+    }
+    }, 10000);
+});
