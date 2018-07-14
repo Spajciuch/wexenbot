3399ac0b67d1323ddbe73ecb864771bdee6403b5
neoney <30625554+heheszeq@users.noreply.github.com>
2018-07-14 02:20:38 +0200

    v12, really...

--- server.js
+++ server.js
@@ -215,7 +215,7 @@ client.on('guildMemberAdd', async member => {
   const avatar = await Canvas.loadImage(buffer);
   ctx.drawImage(avatar, 37, 26, 194, 194);
 
-  const attachment = new Discord.Attachment(canvas.toBuffer(), 'welcome-image.png');
+  const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
 
   channel.send(`Welcome to the server, ${member}!`, attachment);
 });
