6350155ee39286125b23154bd942fd67e5f91dd9
neoney <heheszeqo@gmail>
2018-08-03 23:38:35 +0200

    >lvl fix

--- commands/lvl.js
+++ commands/lvl.js
@@ -2,8 +2,8 @@ const Discord = require('discord.js')
 const firebase = require('firebase')
 module.exports.run = async (client, message, args, config) => {
 const database = firebase.database();
-database.ref("/" + message.author.id + '/xp').once(value).then(xp => {
-  message.reply(xp)
+database.ref("/" + message.author.id + '/xp').once("value").then(xp => {
+  message.reply(xp.val())
 })
 }
 module.exports.help = {
