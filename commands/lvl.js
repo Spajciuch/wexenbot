3ebf9d7c9109eaa99a3a7127ab76266cce718f86
neoney <heheszeqo@gmail>
2018-08-03 23:31:38 +0200

    Yaya

--- commands/lvl.js
+++ commands/lvl.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js')
+const firebase = require('firebase')
 module.exports.run = async (client, message, args, config) => {
 const database = firebase.database();
 database.ref("/" + message.author.id + '/xp').once(value).then(xp => {
