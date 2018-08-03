84633867ed79126ba75c7bc56bc6a1f009cd29f6
neoney <heheszeqo@gmail>
2018-08-03 23:24:56 +0200

    Maciuś lubi małe sityc

--- commands/lvl.js
+++ commands/lvl.js
@@ -4,9 +4,7 @@ const database = firebase.database();
 database.ref("/" + message.author.id + '/xp').once(value).then(xp => {
   message.reply(xp)
 })
-
 }
-message.reply(lvl)
 module.exports.help = {
   name: "lvl",
   category: "info"
