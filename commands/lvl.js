ef6fda2936bc9a1fe3c33124e5f2a809c9fbdbe1
neoney <30625554+heheszeq@users.noreply.github.com>
2018-08-04 13:54:54 +0200

    Update lvl.js

--- commands/lvl.js
+++ commands/lvl.js
@@ -3,7 +3,7 @@ const firebase = require('firebase')
 module.exports.run = async (client, message, args, config) => {
 const database = firebase.database();
 database.ref("/" + message.author.id + '/xp').once("value").then(xp => {
-  message.reply(xp.val() + ", Poziom: " + Math.floor(xp.val() / 400))
+  message.reply(xp.val() + ", Level: " + Math.floor(xp.val() / 400) + ". You need " + (((Math.floor(xp.val() / 400) + 1) * 400) - xp.val()) + " xp to reach level " + (Math.floor(xp.val() / 400) + 1) )
 })
 }
 module.exports.help = {
