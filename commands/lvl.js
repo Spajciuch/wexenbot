c8675be5f42a8ccd82c6b3e110d2e1f80eebd65e
neoney <30625554+heheszeq@users.noreply.github.com>
2018-08-04 13:44:54 +0200

    Update lvl.js

--- commands/lvl.js
+++ commands/lvl.js
@@ -3,7 +3,7 @@ const firebase = require('firebase')
 module.exports.run = async (client, message, args, config) => {
 const database = firebase.database();
 database.ref("/" + message.author.id + '/xp').once("value").then(xp => {
-  message.reply(xp.val())
+  message.reply(xp.val() + ", Poziom: " + Math.floor(xp.val() / 400))
 })
 }
 module.exports.help = {
