69a11cb502a63a209efede083015f8f54a3095f7
neoney <30625554+heheszeq@users.noreply.github.com>
2018-08-19 15:19:04 +0200

    fix tempban

--- commands/tempban.js
+++ commands/tempban.js
@@ -4,17 +4,16 @@ module.exports.run = async (bot, message, args, config) => {
 var firebase = require('firebase')
 await firebase.database().ref(`/ustawienia/${message.guild.id}/admin`).once('value')
 .then(async admin => { if(admin.val() == false) return message.reply('Module is off.');
-console.log(message)
       if (!message.member.hasPermission("KICK_MEMBERS")) return message.reply("You aren't permitted to do that");
     let toban = message.mentions.members.first() || message.guild.members.get(args[0]);
     if (!toban) return message.reply("Couldn't find user.");
     let bantime = args[1];
     if (!bantime) return message.reply("You didn't specify a time!");
-    await (message.guild.ban(toban.id));
+    await (message.guild.members.ban(toban.id));
     message.reply(`<@${toban.id}> has been banned for ${ms(ms(bantime))}`);
 
     setTimeout(function() {
-        message.guild.unban(toban.id)
+        message.guild.members.unban(toban.id)
         message.channel.send(`<@${tomute.id}> has been unbanned!`);
     }, ms(bantime));
                      })
