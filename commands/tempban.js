1151358577830d0a20f290ceeacada663592cdb3
neoney <30625554+heheszeq@users.noreply.github.com>
2018-08-19 15:08:06 +0200

    tempban fixes

--- commands/tempban.js
+++ commands/tempban.js
@@ -5,11 +5,10 @@ var firebase = require('firebase')
 await firebase.database().ref(`/ustawienia/${message.guild.id}/admin`).once('value')
 .then(async admin => { if(admin.val() == false) return message.reply('Module is off.');
       if (!message.member.hasPermission("KICK_MEMBERS")) return message.reply("You aren't permitted to do that");
-    let toban = message.guild.member(message.mentions.members.first() || message.guild.members.get(args[0]));
+    let toban = message.mentions.members.first() || message.guild.members.get(args[0]);
     if (!toban) return message.reply("Couldn't find user.");
     let bantime = args[1];
     if (!bantime) return message.reply("You didn't specify a time!");
-    if(toban.highestRole.position > message.member.highestRole.position) return message.reply('He/She has a higher role than you!');
     await (message.guild.ban(toban.id));
     message.reply(`<@${toban.id}> has been banned for ${ms(ms(bantime))}`);
 
